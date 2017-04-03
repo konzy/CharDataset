6deee063a1ac4edda184d59057af1061b77f601d
Brian <bkonzman@gmail.com>
2017-04-03 18:38:45 -0400

    initial commit

---
+++ TestCharacters.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------------ Imports ----------------------------------#
+
+# Import python imaging libs
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont, ImageOps, ImageFilter
+
+from itertools import chain
+import sys
+
+from fontTools.ttLib import TTFont
+from fontTools.unicode import Unicode
+
+# Import operating system lib
+import os
+import unicodedata
+import shutil
+# Import random generator
+from random import randint
+import random
+
+# ---------------------------------- Input and Output ---------------------------#
+
+# Directory containing fonts
+out_dir = './char'
+
+# Output
+font_dir = './fonts'
+
+# ------------------------------------ Characters -------------------------------#
+
+oriya = ['0x0b01', '0x0b77']
+sinhala = ['0x0d90', '0x0ddf']
+korean = ['0xac00', '0xd7af']
+english = ['0x0030', '0x0039', '0x0041', '0x004e', '0x004e', '0x005b', '0x0061', '0x007a']
+kannada = ['0x0C80', '0x0C81', '0x0C91', '0x0C92', '0x0C9f', '0x0CFF']
+
+
+# -------------------------------------- Sizes ----------------------------------#
+
+# Character sizes
+font_sizes = (48, 52, 56, 64)
+
+# Image size
+image_size = 256
+
+# Images per font file
+num_images = 30
+
+# ------------------------------------ Cleanup ----------------------------------#
+
+# def Cleanup():
+#     shutil.rmtree(out_dir)
+#     os.mkdir(out_dir)
+#     return
+
+# ------------------------------ Generate Characters ----------------------------#
+
+
+def generate_characters(char_start, char_end, script_name):
+    # Process the font files
+    for dir_name, dir_names, file_names in os.walk(font_dir + "/" + script_name + "/"):
+        # For each font do
+        for filename in file_names:
+            # Get font full file path
+            font_resource_file = os.path.join(dir_name, filename)
+
+            # For each character do
+            for char_int in range(int(char_start, 16), int(char_end, 16)):
+                # for char in characters:
+                # For each font size do
+                # ttf = TTFont(charInt, 0, verbose=0, allowVID=0, ignoreDecompileErrors=True, fontNumber=-1)
+                # chars = chain.from_iterable([y + (Unicode[y[0]],) for y in x.cmap.items()]
+                # for x in ttf["cmap"].tables)
+                #
+                # char = int(sys.argv[2], 0)
+                # # print(Unicode[char])
+                # # print(char in (x[0] for x in chars))
+                #
+                # if char in (x[0] for x in chars):
+                for i in range(0, num_images):
+                    font_size = randint(image_size / 5, image_size / 3)
+                    valid_unicode = True
+                    character = unichr(char_int)
+                    try:
+                        unicodedata.name(character)
+                    except ValueError:
+                        valid_unicode = False
+
+                    if valid_unicode:
+                        draw_character(character, font_resource_file, font_size, script_name, filename)
+
+    print script_name + " is finished."
+    return
+
+
+def draw_character(char, font_resource_file, font_size, script_name, filename):
+    if type(char) is int:
+        character = unichr(char)
+    else:
+        character = char
+    # Convert the character into unicode
+    background_color = randint(0, 255)
+    # Create character image :
+    # Grayscale, image size, background color
+    char_image = Image.new('L', (image_size, image_size), background_color)
+    # Specify font : Resource file, font size
+    font = ImageFont.truetype(font_resource_file, font_size)
+
+    # Get character width and height
+    (font_width, font_height) = font.getsize(character)
+
+    # Draw text : Position, String,
+    # Options = Fill color, Font
+    img2 = Image.new('L', (font_width, font_height), background_color)
+    draw2 = ImageDraw.Draw(img2)
+    font_color = (background_color + randint(75, 125)) % 255
+    draw2.text((0, 0), character, font_color, font=font)
+    w = img2.rotate(randint(-25, 25), expand=1)
+    width, height = w.size
+
+    # Calculate x position
+    x = randint(0, image_size - width - 10)
+    # Calculate y position
+    y = randint(0, image_size - height - 10)
+
+    # Final file name
+    file_name = out_dir + "/" + script_name + "/" + character + '_' + \
+        filename + '_fs_' + \
+        str(font_size) + '_bc_' + \
+        str(background_color) + '_fc_' + str(font_color) + '.png'
+
+    # Save image
+    char_image.paste(w, (x, y), w)
+    char_image.save(file_name)
+    # Print character file name
+    # print file_name
+
+    # to create the mishmash of two languages
+
+
+def copy_files(source, destination_1, destination_2):
+    for filename in os.listdir(source):
+        if random.random() < 0.5:
+            shutil.copy(source + "/" + filename, destination_1)
+        else:
+            shutil.copy(source + "/" + filename, destination_2)
+
+# -------------------------------------- Main -----------------------------------#
+
+# Do cleanup
+# Cleanup()
+
+
+# Generate characters
+random.seed(21215)
+# generate_characters(english[2], english[3], "englishTest")
+# generate_characters(english[4], english[5], "englishVal")
+generate_characters(kannada[1], kannada[2], "kannadaTest")
+generate_characters(kannada[3], kannada[4], "kannadaVal")
+
+
+__author__ = 'hensleyl4'
